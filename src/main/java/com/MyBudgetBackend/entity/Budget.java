970e9b98052fee9a53c2a34e2cef7ddf8b963fef
Ethan Gustafson <ethangustafson96@gmail.com>
2021-09-13 17:22:30 -0400

    define attributes, getters and setters for Budget Entity class

--- src/main/java/com/MyBudgetBackend/entity/Budget.java
+++ src/main/java/com/MyBudgetBackend/entity/Budget.java
@@ -1,5 +1,40 @@
 package com.MyBudgetBackend.entity;
 
 public class Budget {
-	
+	private int id;
+	private String name;
+	private int amount;
+	private User user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
