33686efadced145646d8c15ecedfc83fef66d806
Ethan Gustafson <ethangustafson96@gmail.com>
2021-09-13 19:44:46 -0400

    define attributes, getters and setters for Expense Entity class

--- src/main/java/com/MyBudgetBackend/entity/Expense.java
+++ src/main/java/com/MyBudgetBackend/entity/Expense.java
@@ -1,5 +1,51 @@
 package com.MyBudgetBackend.entity;
 
+import java.sql.Date;
+
 public class Expense {
-	
+	private int id;
+	private String name;
+	private int cost;
+	private Date date;
+	private Budget budget;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Budget getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Budget budget) {
+        this.budget = budget;
+    }
 }
