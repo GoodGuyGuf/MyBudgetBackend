718d9559e81d1d774d361714d05f729c179f3d8e
Ethan Gustafson <ethangustafson96@gmail.com>
2021-09-13 19:46:26 -0400

    define attributes, getters and setters for Item Entity class

--- src/main/java/com/MyBudgetBackend/entity/Item.java
+++ src/main/java/com/MyBudgetBackend/entity/Item.java
@@ -1,5 +1,51 @@
 package com.MyBudgetBackend.entity;
 
+import java.sql.Date;
+
 public class Item {
-	
+	private int id;
+	private String name;
+	private int cost;
+	private Date date;
+	private Expense expense;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Expense getExpense() {
+        return expense;
+    }
+
+    public void setExpense(Expense expense) {
+        this.expense = expense;
+    }
 }
