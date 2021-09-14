04e5920bf265ce910351b1b431084353b6a3211f
Ethan Gustafson <ethangustafson96@gmail.com>
2021-09-13 21:12:17 -0400

    add entity annotations, OneToOne & OneToMany relationship with getters and setters in Entity Expense class

--- src/main/java/com/MyBudgetBackend/entity/Expense.java
+++ src/main/java/com/MyBudgetBackend/entity/Expense.java
@@ -1,13 +1,25 @@
 package com.MyBudgetBackend.entity;
 
+import javax.persistence.*;
 import java.sql.Date;
+import java.util.List;
 
+@Entity
 public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String name;
 	private int cost;
 	private Date date;
+
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="budget_id")
 	private Budget budget;
+
+	@OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name="expense_id")
+    private List<Item> items;
 
     public int getId() {
         return id;
@@ -47,5 +59,13 @@ public class Expense {
 
     public void setBudget(Budget budget) {
         this.budget = budget;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
     }
 }
