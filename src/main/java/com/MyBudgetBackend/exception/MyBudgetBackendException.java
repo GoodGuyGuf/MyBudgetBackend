91901615b1ac25387c391f13a51e91f987956c66
Ethan Gustafson <ethangustafson96@gmail.com>
2021-09-22 22:22:14 -0400

    add generic Exception class

---
+++ src/main/java/com/MyBudgetBackend/exception/MyBudgetBackendException.java
@@ -0,0 +1,7 @@
+package com.MyBudgetBackend.exception;
+
+public class MyBudgetBackendException extends Exception {
+    public MyBudgetBackendException(String message) {
+        super(message);
+    }
+}
